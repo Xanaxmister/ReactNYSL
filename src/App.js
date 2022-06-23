aa37aa631e2bdc91858a9fe746407dae0694a0ad
xamaxmesiter <xanaxmeister@idonthave.com>
2022-06-23 20:14:24 +0200

    firstFeel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Switch, Routes } from "react-router-dom";
+
+const Home = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+};
+
+
+const Category = () => (
+  <div>
+    <h2>Category</h2>
+  </div>
+);
+
+const Products = () => (
+  <div>
+    <h2>Products</h2>
+  </div>
+);
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="navbar navbar-light">
+        <ul className="nav navbar-nav">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/category">Category</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </nav>
+
+      { /* Route components are rendered if the path prop matches the current URL */}
+      <Routes>
+      <Route path="/"><Home /></Route>
+      <Route path="/category"><Category /></Route>
+      <Route exact path="/products"><Products /></Route>
+      </Routes>
     </div>
   );
 }
