5ccb13333798a568faea9465fcd5ec64ab222907
xamaxmesiter <xanaxmeister@idonthave.com>
2022-06-23 20:42:39 +0200

    U used wrong version dummy

--- src/App.js
+++ src/App.js
@@ -1,57 +1,18 @@
-import './App.css';
-import { Link, Route, Switch, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Home = (props) => {
-  console.log(props);
-
+export default function App() {
   return (
     <div>
-      <h2>Home</h2>
-    </div>
-  );
-};
-
-
-const Category = () => (
-  <div>
-    <h2>Category</h2>
-  </div>
-);
-
-const Products = () => (
-  <div>
-    <h2>Products</h2>
-  </div>
-);
-
-function App() {
-
-
-
-  return (
-    <div>
-      <nav className="navbar navbar-light">
-        <ul className="nav navbar-nav">
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/category">Category</Link>
-          </li>
-          <li>
-            <Link to="/products">Products</Link>
-          </li>
-        </ul>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
       </nav>
-
-      { /* Route components are rendered if the path prop matches the current URL */}
-      <Routes>
-      <Route path="/"><Home /></Route>
-      <Route path="/category"><Category /></Route>
-      <Route exact path="/products"><Products /></Route>
-      </Routes>
     </div>
   );
 }
-
-export default App;
